e3f05676a0c2b4d5f916f35017af412199c5be26
Vorfeed1 <evorfeed@gmail.com>
2014-03-28 07:26:28 -0700

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2012 The Bitcoin developers
 // Copyright (c) 2012 Litecoin Developers
+// Copyright (c) 2013-2014 Bellscoin Developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include <string>
@@ -9,7 +10,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("Bells");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   "-beta"
