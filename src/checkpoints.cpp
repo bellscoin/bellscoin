52dd9ee89b7dcb58d4a8aef6cc552ad9e51843e7
Vorfeed1 <evorfeed@gmail.com>
2014-03-28 07:20:10 -0700

    Update checkpoints.cpp
    New Checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -28,6 +28,15 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
             boost::assign::map_list_of
             (  0, uint256("0xe5be24df57c43a82d15c2f06bda961296948f8f8eb48501bed1efb929afe0698"))
+            (  1000, uint256("0xbda6660ca8dedd6205bf1afd68b1ee0479c293899d308325bbf921adaeafc459"))
+            (  10000, uint256("0x6672369ac18c1597d9d062b10847cc92d6fba6218a63291f258450bae789f774"))
+            (  25000, uint256("0x2910fc9061c1d458b3104fc7cb3fa3c23dbc8370af203c683a5d09e2f062c0cd"))
+            (  50000, uint256("0xee8a74953afdae365ec3d2bc003590b594bd51b2f10e25be5d1960f1e12c6609"))
+            (  75000, uint256("0x133b93343f032c06dcbd4b2607f0427fca1a1940279b9d1f9ef1475a37862f9e"))
+            (  100000, uint256("0x707c4d80b012f540d1ee4021ad4214a187e7197504f5a0b67b4ae87e2491ff4c"))
+            (  110000, uint256("0x4e903f691d8434af6a81091e2ddd832fd74aefd5acc79837bc605b183ddffdb3"))
+            (  125000, uint256("0xddc37d2f69cc22dd3ffe482442821cee466b899fa706081bc7f425ab66761f26"))
+            (  134000, uint256("0x951580c3396b2058eb8f38cde780dc57cf2ec511b954a18f54beb8f4652df35f"))
 			;
 
 
